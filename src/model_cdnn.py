4438fbf3b06a3beec688b87a62d1c73e955785aa
caijinhong1999 <cjh18242065873@163.com>
2025-04-05 11:51:15 +1100

    实现基础框架（数据读取，模型构建，模型评估，模型可视化）；实现cdnn 9 层和12层神经网络

---
+++ src/model_cdnn.py
@@ -0,0 +1,69 @@
+import torch.nn as nn
+# 构建 9 层卷积神经网络
+class NineLayerCNN(nn.Module):
+    def __init__(self):
+        super(NineLayerCNN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)
+        self.relu1 = nn.ReLU()
+        self.pool1 = nn.MaxPool2d(2)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.relu2 = nn.ReLU()
+        self.pool2 = nn.MaxPool2d(2)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.relu3 = nn.ReLU()
+        self.pool3 = nn.MaxPool2d(2)
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(128 * 6 * 6, 128)
+        self.relu4 = nn.ReLU()
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = self.pool1(self.relu1(self.conv1(x)))
+        x = self.pool2(self.relu2(self.conv2(x)))
+        x = self.pool3(self.relu3(self.conv3(x)))
+        x = self.flatten(x)
+        x = self.relu4(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+
+# 构建 12 层卷积神经网络
+class TwelveLayerCNN(nn.Module):
+    def __init__(self):
+        super(TwelveLayerCNN, self).__init__()
+        # 第一层卷积块
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)
+        self.relu1 = nn.ReLU()
+        self.pool1 = nn.MaxPool2d(2)
+        # 第二层卷积块
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.relu2 = nn.ReLU()
+        self.pool2 = nn.MaxPool2d(2)
+        # 第三层卷积块
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.relu3 = nn.ReLU()
+        self.pool3 = nn.MaxPool2d(2)
+        # 新增第四层卷积块
+        self.conv4 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
+        self.relu4 = nn.ReLU()
+        self.pool4 = nn.MaxPool2d(2)
+        self.flatten = nn.Flatten()
+        # 新增全连接层
+        self.fc1 = nn.Linear(256 * 3 * 3, 256)
+        self.relu5 = nn.ReLU()
+        self.fc2 = nn.Linear(256, 128)
+        self.relu6 = nn.ReLU()
+        self.fc3 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = self.pool1(self.relu1(self.conv1(x)))
+        x = self.pool2(self.relu2(self.conv2(x)))
+        x = self.pool3(self.relu3(self.conv3(x)))
+        x = self.pool4(self.relu4(self.conv4(x)))
+        x = self.flatten(x)
+        x = self.relu5(self.fc1(x))
+        x = self.relu6(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
